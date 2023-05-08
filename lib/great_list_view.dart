04c94d07e755f9b5e82dd7e064596339f33ce9f2
willroymyles <willroymyles@yahoo.com>
2023-05-08 04:06:41 -0500

    Changed morphComparator type from Widget to dynamic

--- lib/great_list_view.dart
+++ lib/great_list_view.dart
@@ -2,4 +2,5 @@ library great_list_view;
 
 export 'package:great_list_view/src/core.dart';
 export 'package:great_list_view/src/dispatcher.dart';
+export 'package:great_list_view/src/morph_transition.dart';
 export 'package:worker_manager/worker_manager.dart';
